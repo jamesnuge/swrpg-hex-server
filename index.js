2bb4441c62bb19530bc2c1f55bfaf5e8a60dacc1
James Nugent <james.nugent89@gmail.com>
2020-10-02 22:53:48 +1000

    Added WS flux server. Created session sotre with actions INIT, CONNECT and SHUTDOWN

--- index.js
+++ index.js
@@ -2,10 +2,45 @@ const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 8080 });
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-  });
+const sessionStore = {};
 
-  ws.send('something');
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(rawMessage) {
+    const {sessionId, type} = JSON.parse(rawMessage);
+    const session = sessionStore[sessionId];
+    if (session) {
+      switch (type) {
+        case 'SHUTDOWN':
+          console.log(`shutting down session: ${sessionId}`);
+          session.host.close(1000, 'User initiated shutdown');
+          session.members.forEach((memberWs) => memberWs.close(1000, 'Host shutdown the session'))
+          delete session[sessionId];
+        default:
+          console.log('Sending message to members...');
+          console.log(JSON.stringify(message, null, 2));
+          session.members.forEach((memberWs) => memberWs.send(message))
+      }
+    } else {
+      switch(type) {
+        case 'INIT': 
+          const state = message.state;
+          ws.send('initialized');
+          sessionStore[sessionId] = createSessionDetails(ws);
+          break;
+        case 'CONNECT':
+          const hostSession = sessionStore[message.host];
+          hostSession.members.push(ws);
+        default:
+      }
+    }
+  });
+  ws.send('Connected to flux server');
 });
+
+function createSessionDetails(host, state) {
+  return {
+    host,
+    state,
+    members: []
+  };
+}
