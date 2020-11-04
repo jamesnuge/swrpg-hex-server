c9d4addecd07f40953555b4e2a9634c49d00d55e
James Nugent <james.nugent89@gmail.com>
2020-11-04 22:58:35 +1100

    Added envrc. Made server throw if no port is defined

--- index.js
+++ index.js
@@ -1,109 +1,116 @@
 const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 3001 });
+const PORT = process.env.SERVER_PORT;
+if (PORT) {
 
-const sessionStore = {};
+  const wss = new WebSocket.Server({ port: PORT });
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(rawMessage) {
-    const message = JSON.parse(rawMessage);
-    const { user, type, payload } = message;
-    const {sessionId, userId} = user;
-    const session = sessionStore[sessionId];
-    console.log('received message with sessionId: ', sessionId);
-    console.log('-> of type: ', type);
-    if (session) {
-      console.log('-> has session')
-      switch (type) {
-        case 'SHUTDOWN':
-          session.host.close(1000, 'User initiated shutdown');
-          session.members.forEach((memberWs) => memberWs.close(1000, 'Host shutdown the session'))
-          delete session[sessionId];
-          break;
-        case 'HEX_SELECTED':
-          console.log('-> HEX_SELECTED')
-          console.log('userId:', userId);
-          console.log('sessionId:', sessionId);
-          const messageWithUserDetails = withUserDetails(message, userId);
-          if (userId === sessionId) {
-            console.log('   -> is the host')
-            session.members.forEach((socket) => socket.send(messageWithUserDetails));
-          } else {
-            console.log('   -> is not the host')
-            session.host.send(JSON.stringify(messageWithUserDetails));
-            session.members
-              .filter((member) => member.id !== userId)
-              .forEach((member) => member.send(messageWithUserDetails));
-          }
-          if (session.state.selectedHex) {
-            session.state.selectedHex = undefined;
-          } else {
-            session.state.selectedHex = message.payload;
-          }
-          break;
-        case 'JOIN_SESSION':
-          console.log(' -> processing join session')
-          const {members} = sessionStore[message.user.sessionId];
-          const memberToAdd = {
-            id: userId,
-            send: (message) => {
-              if (typeof message === 'object') {
-                const rawMessage = JSON.stringify(message)
-                console.log('Sending message to member: ', message)
-                ws.send(rawMessage);
-              } else if (typeof message === 'string') {
-                ws.send(message);
-                console.log('Sending message to member: ', message)
-              } else {
-                console.warn('Cannot send message of type: ', typeof message);
+  const sessionStore = {};
+
+  wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(rawMessage) {
+      const message = JSON.parse(rawMessage);
+      const { user, type, payload } = message;
+      const { sessionId, userId } = user;
+      const session = sessionStore[sessionId];
+      console.log('received message with sessionId: ', sessionId);
+      console.log('-> of type: ', type);
+      if (session) {
+        console.log('-> has session')
+        switch (type) {
+          case 'SHUTDOWN':
+            session.host.close(1000, 'User initiated shutdown');
+            session.members.forEach((memberWs) => memberWs.close(1000, 'Host shutdown the session'))
+            delete session[sessionId];
+            break;
+          case 'HEX_SELECTED':
+            console.log('-> HEX_SELECTED')
+            console.log('userId:', userId);
+            console.log('sessionId:', sessionId);
+            const messageWithUserDetails = withUserDetails(message, userId);
+            if (userId === sessionId) {
+              console.log('   -> is the host')
+              session.members.forEach((socket) => socket.send(messageWithUserDetails));
+            } else {
+              console.log('   -> is not the host')
+              session.host.send(JSON.stringify(messageWithUserDetails));
+              session.members
+                .filter((member) => member.id !== userId)
+                .forEach((member) => member.send(messageWithUserDetails));
+            }
+            if (session.state.selectedHex) {
+              session.state.selectedHex = undefined;
+            } else {
+              session.state.selectedHex = message.payload;
+            }
+            break;
+          case 'JOIN_SESSION':
+            console.log(' -> processing join session')
+            const { members } = sessionStore[message.user.sessionId];
+            const memberToAdd = {
+              id: userId,
+              send: (message) => {
+                if (typeof message === 'object') {
+                  const rawMessage = JSON.stringify(message)
+                  console.log('Sending message to member: ', message)
+                  ws.send(rawMessage);
+                } else if (typeof message === 'string') {
+                  ws.send(message);
+                  console.log('Sending message to member: ', message)
+                } else {
+                  console.warn('Cannot send message of type: ', typeof message);
+                }
               }
             }
-          }
-          members.push(memberToAdd);
-          memberToAdd.send({
-            type: 'INITIALIZE_BOARD',
-            payload: {
-              radius: session.radius
-            }
-          });
-          if (session.state.selectedHex) {
+            members.push(memberToAdd);
             memberToAdd.send({
-              type: 'HEX_SELECTED',
-              payload: session.state.selectedHex,
-              origin: sessionId
-            })
-          }
-          break;
-        default:
-          console.log('Unconfigured message type detected...');
-          console.log('message: ');
-          console.log(JSON.stringify(message, null, 2));
+              type: 'INITIALIZE_BOARD',
+              payload: {
+                radius: session.radius
+              }
+            });
+            if (session.state.selectedHex) {
+              memberToAdd.send({
+                type: 'HEX_SELECTED',
+                payload: session.state.selectedHex,
+                origin: sessionId
+              })
+            }
+            break;
+          default:
+            console.log('Unconfigured message type detected...');
+            console.log('message: ');
+            console.log(JSON.stringify(message, null, 2));
+        }
+      } else {
+        switch (type) {
+          case 'INITIALIZE_BOARD':
+            ws.send('initialized');
+            sessionStore[sessionId] = createSessionDetails(ws, payload.radius);
+            break;
+          default:
+        }
       }
-    } else {
-      switch (type) {
-        case 'INITIALIZE_BOARD':
-          ws.send('initialized');
-          sessionStore[sessionId] = createSessionDetails(ws, payload.radius);
-          break;
-        default:
-      }
-    }
+    });
+    ws.send('Connected to flux server');
   });
-  ws.send('Connected to flux server');
-});
 
-function createSessionDetails(host, radius) {
-  return {
-    host,
-    state: {},
-    members: [],
-    radius
-  };
-}
-
-const withUserDetails = (message, userId) => {
-  return {
-    ...message,
-    origin: userId
+  function createSessionDetails(host, radius) {
+    return {
+      host,
+      state: {},
+      members: [],
+      radius
+    };
   }
+
+  const withUserDetails = (message, userId) => {
+    return {
+      ...message,
+      origin: userId
+    }
+  }
+
+} else {
+  throw new Error(`Misconfiguration: Server port env variable must be defined`)
 }
